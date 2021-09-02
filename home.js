ec13a6d3498b00386116d44f482d405a18923a3e
jamesdoranservicensw <88362216+jamesdoranservicensw@users.noreply.github.com>
2021-09-02 14:21:03 +1000

    Create home.js

---
+++ home.js
@@ -0,0 +1,67 @@
+import './style.css';
+function Home() {
+    return (
+        <>
+        <div id className="info-container">
+            <h1>
+                Information about Project
+            </h1>
+            </div>
+            <div id className="nsw-container">
+
+                <section>
+
+                    <h2>NSW Generations Fund</h2>
+                    
+                        <p>Our sovereign wealth fund, the NSW Generations Fund (NGF), continues to grow. <br></br>
+                            Seeded with an initial investment of $10 billion in late 2019, the NGF was valued at $10.9 billion at 30 June 2020, and is projected to grow to more than $30 billion over the next decade. This initiative will help lower the debt burden for future generations. <br></br>
+                            Since the Budget, the Government has directed nearly $300 million extra into the NGF. The first community project funded by the NSW Generations fund was My Community Project.<br></br>
+                            The fund helps keep the state’s debt at a sustainable level, while sharing the benefits of its returns with the people of NSW by funding community projects.<br></br>
+                            The fund helps keep the state’s debt at a sustainable level, while sharing the benefits of its returns with the people of NSW by funding community projects. <br></br></p>
+                        Successful projects align with one or more of the five program categories :
+                        <ul>
+                            <li>Accessible communities</li>
+                            <li>Cultural communities</li>
+                            <li>Healthy communities</li>
+                            <li>Liveable communities</li>
+                            <li>Safe communities</li>
+
+                        </ul>
+                   
+                </section>
+
+                <section>
+
+                <img src="/community.png" width="400" height="300"></img>
+
+                <img src="/community 2.png" width="400" height="300"></img>
+
+                </section>
+                
+
+                </div>
+
+                 <div id className="eligibility-container" >
+                    <section>
+                        
+
+                            <h2>
+                                Eligibility
+                            </h2>
+
+                            <ul>
+                                <li>Customers must reside in NSW and can only vote once</li>
+                                <li>Customers must provide email address</li>
+                            </ul>
+                       
+                    </section>
+
+
+
+                </div>
+        </>
+
+            );
+}
+
+            export default Home;
